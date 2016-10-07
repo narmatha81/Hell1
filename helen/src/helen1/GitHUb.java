005b0e540235b6eb81bb6ec1e7cb8436635b197c
nancy <nancy@gmail.com>
2016-10-06 22:11:51 -0400

    committed helen project

---
+++ helen/src/helen1/GitHUb.java
@@ -0,0 +1,12 @@
+package helen1;
+
+public class GitHUb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("welcome to first git project");
+
+	}
+
+}
